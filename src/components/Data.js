fc052b321217155836b980712896b0aec21c8310
rachit <parmarrachit5@gmail.com>
2024-10-15 05:04:36 +0530

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,65 @@
+ const  Data = [
+  {
+      q:"What is JavaScript ?",
+      a:"JS is Programming language",
+      b:"JS is Framework",
+      c:"None",
+      d:"Typed Langauge",
+      ans:"JS is Programming language"
+  },
+  {
+      q:"What is ReactJS ?",
+      a:"Library",
+      b:"React is a JavaScript Library",
+      c:"Framework",
+      d:"Lnaguage",
+      ans:"React is a JavaScript Library"
+  },
+  {
+      q:"Full Form of JS ?",
+      a:"JavaSell",
+      b:"JavaSpring",
+      c:"JavaScript",
+      d:"JavaSplit",
+      ans:"JavaScript"
+  },
+  {
+      q:"Who Develope JS ?",
+      a:"Brendon ",
+      b:"Brendon Mac",
+      c:"Bretly",
+      d:"Brendon Eich",
+      ans:"Brendon Eich"
+  },
+  {
+      q:"Who Develope ReactJS?",
+      a:"Jordan Walk",
+      b:"Jordan ",
+      c:"Jordan King",
+      d:"Jordan sen",
+      ans:"Jordan Walk"
+
+  },
+  {
+    q: "What is the capital of France?",
+    a:"Berlin",
+    b:"Madrid ",
+    c:"Paris",
+    d:"Rome",
+    ans:"Paris"
+   
+  },
+  {
+    q: "Which planet is known as the Red Planet?",
+    a:"Earth",
+    b:"Mars ",
+    c:"Jupiter",
+    d:"Venus",
+    ans:"Mars"
+
+   
+  },
+  
+]
+
+export default Data;
